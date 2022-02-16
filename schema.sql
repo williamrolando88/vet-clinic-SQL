9fb743b6641423bb259d6a25f4afcac66d8b32cf
williamrolando88 <william.rolando88@hotmail.com>
2022-02-16 16:39:41 -0500

    Add: M2M tables handlers

--- schema.sql
+++ schema.sql
@@ -29,7 +29,6 @@ CREATE TABLE owners (
 	name VARCHAR(50) NOT NULL,
 	age INT NOT NULL
 );
-
 -- Create a table named species with the following columns:
 --   id: integer (set it as autoincremented PRIMARY KEY)
 --   name: string
@@ -37,7 +36,6 @@ CREATE TABLE species (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL
 );
-
 -- Modify animals table:
 --   Make sure that id is set as autoincremented PRIMARY KEY
 --   Remove column species
@@ -54,3 +52,34 @@ ALTER TABLE animals
 	ADD CONSTRAINT animal_owner_id_fk
 	FOREIGN KEY (owner_id)
 	REFERENCES owners(id);
+
+-- *****************************************************************************
+/* Join table for visits */
+
+-- Create a table named vets with the following columns:
+-- 	id: integer (set it as autoincremented PRIMARY KEY)
+-- 	name: string
+-- 	age: integer
+-- 	date_of_graduation: date
+CREATE TABLE vet (
+  id BIGSERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
+  date_of_graduation DATE NOT NULL
+);
+-- There is a many-to-many relationship between the tables species and vets: a vet can specialize in multiple species, and a species can have multiple vets specialized in it. Create a "join table" called specializations to handle this relationship.
+CREATE TABLE specialization(
+  vet_id INT NOT NULL ,
+  species_id INT NOT NULL,
+  PRIMARY KEY (vet_id, species_id),
+  CONSTRAINT vet_spec_rkey FOREIGN KEY (vet_id) REFERENCES vet(id),
+  CONSTRAINT species_spec_rkey FOREIGN KEY (species_id) REFERENCES species(id)
+);
+-- There is a many-to-many relationship between the tables animals and vets: an animal can visit multiple vets and one vet can be visited by multiple animals. Create a "join table" called visits to handle this relationship, it should also keep track of the date of the visit.
+CREATE TABLE visits (
+  animal_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  visit_date DATE NOT NULL,
+  CONSTRAINT animal_visit_fkey FOREIGN KEY (animal_id) REFERENCES animals(id),
+  CONSTRAINT vet_visit_fkey FOREIGN KEY (vet_id) REFERENCES vet(id)
+);
