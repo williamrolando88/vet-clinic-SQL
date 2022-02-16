79b04c5c1ffecc8f0f8784fc917eceb1dfb09f79
williamrolando88 <william.rolando88@hotmail.com>
2022-02-16 16:43:08 -0500

    Add: Relational data for visits and specializations

--- data.sql
+++ data.sql
@@ -84,3 +84,74 @@ UPDATE animals SET owner_id = 10
 UPDATE animals SET owner_id = 11 WHERE name = 'Angemon' or name = 'Boarmon';
 COMMIT;
 
+-- *****************************************************************************
+/* Join table for visits */
+
+-- Insert the following data for vets:
+-- 	Vet William Tatcher is 45 years old and graduated Apr 23rd, 2000.
+-- 	Vet Maisy Smith is 26 years old and graduated Jan 17th, 2019.
+-- 	Vet Stephanie Mendez is 64 years old and graduated May 4th, 1981.
+-- 	Vet Jack Harkness is 38 years old and graduated Jun 8th, 2008.
+BEGIN;
+INSERT INTO vet (name,age,date_of_graduation) 
+  VALUES ('William Tatcher', 45, '2000/04/23');
+INSERT INTO vet (name,age,date_of_graduation) 
+  VALUES ('Maisy Smith', 26, '2019/01/17');
+INSERT INTO vet (name,age,date_of_graduation) 
+  VALUES ('Stephanie Mendez', 64, '1981/05/04');
+INSERT INTO vet (name,age,date_of_graduation) 
+  VALUES ('Jack Harness', 38, '2008/06/08');
+COMMIT;
+-- Insert the following data for specialties:
+-- 	Vet William Tatcher is specialized in Pokemon.
+-- 	Vet Stephanie Mendez is specialized in Digimon and Pokemon.
+-- 	Vet Jack Harkness is specialized in Digimon.
+BEGIN;
+INSERT INTO specialization VALUES (1,3);
+INSERT INTO specialization VALUES (3,3);
+INSERT INTO specialization VALUES (3,4);
+INSERT INTO specialization VALUES (4,4);
+COMMIT;
+-- Insert the following data for visits:
+-- 	Agumon visited William Tatcher on May 24th, 2020.
+-- 	Agumon visited Stephanie Mendez on Jul 22th, 2020.
+-- 	Gabumon visited Jack Harkness on Feb 2nd, 2021.
+-- 	Pikachu visited Maisy Smith on Jan 5th, 2020.
+-- 	Pikachu visited Maisy Smith on Mar 8th, 2020.
+-- 	Pikachu visited Maisy Smith on May 14th, 2020.
+-- 	Devimon visited Stephanie Mendez on May 4th, 2021.
+-- 	Charmander visited Jack Harkness on Feb 24th, 2021.
+-- 	Plantmon visited Maisy Smith on Dec 21st, 2019.
+-- 	Plantmon visited William Tatcher on Aug 10th, 2020.
+-- 	Plantmon visited Maisy Smith on Apr 7th, 2021.
+-- 	Squirtle visited Stephanie Mendez on Sep 29th, 2019.
+-- 	Angemon visited Jack Harkness on Oct 3rd, 2020.
+-- 	Angemon visited Jack Harkness on Nov 4th, 2020.
+-- 	Boarmon visited Maisy Smith on Jan 24th, 2019.
+-- 	Boarmon visited Maisy Smith on May 15th, 2019.
+-- 	Boarmon visited Maisy Smith on Feb 27th, 2020.
+-- 	Boarmon visited Maisy Smith on Aug 3rd, 2020.
+-- 	Blossom visited Stephanie Mendez on May 24th, 2020.
+-- 	Blossom visited William Tatcher on Jan 11th, 2021.
+BEGIN;
+INSERT INTO visits VALUES (1,1,'2020/05/24');
+INSERT INTO visits VALUES (1,3,'2020/07/22');
+INSERT INTO visits VALUES (2,4,'2021/02/21');
+INSERT INTO visits VALUES (3,2,'2020/01/05');
+INSERT INTO visits VALUES (3,2,'2020/03/08');
+INSERT INTO visits VALUES (3,2,'2020/05/14');
+INSERT INTO visits VALUES (4,3,'2021/05/04');
+INSERT INTO visits VALUES (5,4,'2021/02/24');
+INSERT INTO visits VALUES (6,2,'2019/12/21');
+INSERT INTO visits VALUES (6,1,'2020/08/10');
+INSERT INTO visits VALUES (6,2,'2021/04/07');
+INSERT INTO visits VALUES (7,3,'2019/09/29');
+INSERT INTO visits VALUES (8,4,'2020/10/03');
+INSERT INTO visits VALUES (8,4,'2020/11/04');
+INSERT INTO visits VALUES (9,2,'2019/01/24');
+INSERT INTO visits VALUES (9,2,'2019/05/15');
+INSERT INTO visits VALUES (9,2,'2020/02/27');
+INSERT INTO visits VALUES (9,2,'2020/08/03');
+INSERT INTO visits VALUES (10,3,'2020/05/24');
+INSERT INTO visits VALUES (10,1,'2021/01/11');
+COMMIT;
--- queries.sql
+++ queries.sql
@@ -126,3 +126,17 @@ SELECT owners.name, count(animals.owner_id)
   ON animals.owner_id = owners.id
   GROUP BY owners.name
   ORDER BY count(animals.owner_id) DESC;
+
+-- *****************************************************************************
+/* Join table for visits */
+
+-- Write queries to answer the following:
+--   Who was the last animal seen by William Tatcher?
+--   How many different animals did Stephanie Mendez see?
+--   List all vets and their specialties, including vets with no specialties.
+--   List all animals that visited Stephanie Mendez between April 1st and August 30th, 2020.
+--   What animal has the most visits to vets?
+--   Who was Maisy Smith's first visit?
+--   Details for most recent visit: animal information, vet information, and date of visit.
+--   How many visits were with a vet that did not specialize in that animal's species?
+--   What specialty should Maisy Smith consider getting? Look for the species she gets the most.
