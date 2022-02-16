67dfd865ff9d53bea8af8a0e9e9bfa552f13c7e4
williamrolando88 <william.rolando88@hotmail.com>
2022-02-15 21:41:17 -0500

    Add: Queries for requested actions

--- queries.sql
+++ queries.sql
@@ -1,25 +1,80 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+-- *****************************************************************************
+/* Create animals table */
+
 -- Find all animals whose name ends in "mon".
 SELECT * FROM animals WHERE name LIKE '%mon';
-
 -- List the name of all animals born between 2016 and 2019.
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
-
 -- List the name of all animals that are neutered and have less than 3 escape attempts.
 SELECT name FROM animals WHERE neutered = true AND escape_attemps < 3;
-
 -- List date of birth of all animals named either "Agumon" or "Pikachu".
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-
 -- List name and escape attempts of animals that weigh more than 10.5kg
 SELECT name, escape_attemps FROM animals WHERE weight_kg > 10.5;
-
 -- Find all animals that are neutered.
 SELECT * FROM animals WHERE neutered = true;
-
 -- Find all animals not named Gabumon.
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
-
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <=17.3;
+
+-- *****************************************************************************
+/* Query and update animals table */
+
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before transaction.
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Inside a transaction: Update the animals table by setting the species column to digimon for all animals that have a name ending in mon. Update the animals table by setting the species column to pokemon for all animals that don't have species already set. Commit the transaction. Verify that change was made and persists after commit.
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species <> 'digimon';
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction. After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
+BEGIN;
+DELETE FROM animals WHERE id IS NOT NULL;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
+-- Create a savepoint for the transaction.
+-- Update all animals' weight to be their weight multiplied by -1.
+-- Rollback to the savepoint
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+-- Commit transaction
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg* - 1;
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg* - 1 WHERE weight_kg < 0;
+COMMIT;
+
+-- Write queries to answer the following questions:
+-- How many animals are there?
+SELECT count(id) FROM animals;
+-- How many animals have never tried to escape?
+SELECT count(id) FROM animals WHERE escape_attemps = 0;
+-- What is the average weight of animals?
+SELECT avg(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, max(escape_attemps) FROM animals GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, max(weight_kg), min(weight_kg) 
+  FROM animals 
+  GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, avg(escape_attemps) 
+  FROM animals
+  WHERE date_of_birth >= '1990/01/01' AND date_of_birth <= '2000/01/01'
+  GROUP BY species;
